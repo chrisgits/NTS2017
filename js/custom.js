a2d4026c75af4d67bb514046e10f32ff5ffa35dd
Chris Mannel <cmannel77@gmail.com>
2017-02-15 09:50:37 -0600

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+/* Pixel Engineering Co -- Custom js -- Handcrafted Code -- */
+$(document).ready(function() {
+
+  //auto copyright date function
+  var today = new Date();
+  var year = today.getFullYear();
+  var el = document.getElementById('year');
+  el.innerHTML = year;
+
+  //nav-to-top arrow function
+  var offset = 400;
+  var duration = 300;
+  $(window).scroll(function() {
+  if (jQuery(this).scrollTop() > offset) {
+    jQuery('.back_to_top').fadeIn(duration);
+    } else {
+      jQuery('.back_to_top').fadeOut(duration);
+    }
+  });
+
+  $('.back_to_top').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, duration);
+    return false;
+  });
+
+  //smooth scroll fuction for hash links
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 59
+        }, 1000);
+        return false;
+      }
+    }
+  }); //smooth scroll function
+
+});
